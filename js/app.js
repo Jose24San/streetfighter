97684d2b537fd2cb4ad397f006ebf5d52f1b0099
Jose24San <Sanchez24jose@gmail.com>
2016-01-22 19:17:35 -0600

    Added hadoken movement with the audio file playing when ryu shoots it out of his hand.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,40 @@
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();// play song 
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate( /*placement of beginning */
+            {'left': '1020px'},
+            500,
+            function(){/*Ater animation is over put it back at the beginning */
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+    .mouseup(function(){// ryu goes back to his ready position
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    })
+    .keydown(function( event){
+    	if ( event.which == 88){
+    		playHadouken();
+    	}
+    	
+    });
 
-$(".ryu").mouseenter(function() {
-	$(".ryu-still").hide();
-	$(".ryu-ready").show();
-})
-.mouseleave(function() {
-	$(".ryu-ready").hide();
-	$(".ryu-still").show();
-})
-.mousedown(function() {
-	//play hadouken music
-	$(".ryu-ready").hide();
-	$(".ryu-throwing").show();
-	$(".hadouken").show();
-    // animate hadouken it to the right of the screen
-})
-.mouseup(function(){
-	$("ryu-throwing").hide();
-	$("ryu-ready").show();
-	// ryu goes back to his ready position
+
 });
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
