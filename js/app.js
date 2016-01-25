99f37c5075fe517c319d3109792be2fd84d1289f
Jose24San <Sanchez24jose@gmail.com>
2016-01-24 19:12:12 -0600

    Added attacks for Ryu and wrote up new instructions for both character with different keycodes to add a more natural feel when playing with the characters.

--- js/app.js
+++ js/app.js
@@ -38,17 +38,16 @@ function doIntro() {
                         $('.ryu-ready').fadeIn(1000);
                         $('.ryu-instructions').fadeIn(1000);
                         $('.ken-left-stance').fadeIn(1000);
+                        $(".ken-instructions").fadeIn(1000);
                     });
                 }) 
-            });
+            })
           })
         })
       })
     })
   })
 }
-
-
 
 //$(document).ready(function(){
     doIntro();
@@ -65,11 +64,12 @@ function doIntro() {
         $('.ryu-action').hide();
         $('.ryu-throwing').show();
         $('.hadouken').finish().show().animate( /*placement of beginning */
-            {'left': '820px'},
+            {'left': '51.25rem'},
             500,
             function(){/*Ater animation is over put it back at the beginning */
+                
+                $(this).css('left', '23.625rem');
                 $(this).hide();
-                $(this).css('left', '378px');
             });
     })
     .mouseup(function(){// ryu goes back to his ready position
@@ -79,23 +79,23 @@ function doIntro() {
 
     // Ryu animation
     $(document).keydown(function(event){
-    	if (event.keyCode == 88){// x
+    	if (event.keyCode === 65){// a
     		playCool();
     		$(".ryu-action").hide();
     		$(".ryu-cool").show();
-    	} else if ( event.keyCode === 75){// k
+    	} else if ( event.keyCode === 83){// s
             $(".ryu-action").hide();
             $(".ryu-kick").show();
-        } else if ( event.keyCode === 80){// p
+        } else if ( event.keyCode === 68){// d
             $(".ryu-action").hide();
             $(".ryu-punch").show();
             console.log("punch");
-        } else if ( event.keyCode === 74) {// j
+        } else if ( event.keyCode === 70) {// f
             $(".ryu-action").hide();
             $(".ryu-stepping-kick").show();
         }
     }).keyup(function(event){
-    	if (event.keyCode === 88 || event.keyCode === 75 || event.keyCode === 80 || event.keyCode === 74) {
+    	if (event.keyCode === 65 || event.keyCode === 83 || event.keyCode === 68 || event.keyCode === 70) {
             $("#Toulouse-song")[0].pause();
             $("#Toulouse-song")[0].load();
             $(".ryu-action").hide();
@@ -106,25 +106,33 @@ function doIntro() {
 
     // Ken animation 
     $(document).keydown(function(event){
-        if (event.keyCode === 69){ // 69 = e 
-            $(".action").hide();
-            $(".ken-ready").show();
-        } else if ( event.keyCode === 70){ // 70 = f 
-            $(".action").hide();
-            $(".ken-throwing").show();
-            $('.hadouken').finish().show().animate( /*placement of beginning */
-            {'left': '820px'},
+        if (event.keyCode === 74){ // j 
+            $(".ken-action").hide();
+            $(".ken-standing-kick").show();
+        } else if ( event.keyCode === 75){ // k
+            playHadouken();
+            $(".ken-action").hide();
+            $(".ken-throwing-left").show();
+            $('.hadouken-left').finish().show().animate( /*placement of beginning */
+            {'right': '55rem'},
             500,
             function(){/*Ater animation is over put it back at the beginning */
+                
+                $(this).css('right', '22rem');
                 $(this).hide();
-                $(this).css('left', '378px');
             });
-        } 
+        } else if ( event.keyCode === 76) { // l
+            $(".ken-action").hide();
+            $(".ken-shinryuken").show();
+        } else if ( event.keyCode === 186){ // ;
+            $(".ken-action").hide();
+            $(".ken-kick").show();
+        }
     })
     .keyup(function(){// ken goes back to his ready position
-        if ( event.keyCode === 69 || event.keyCode === 70){
-            $('.ken-throwing').hide();
-            $('.ken-ready').show();
+        if ( event.keyCode === 74 || event.keyCode === 75 || event.keyCode === 76 || event.keyCode === 186){
+            $('.ken-action').hide();
+            $('.ken-left-stance').show();
         }
         
     });
